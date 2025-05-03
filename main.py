130c9a05cfdb9fcce9f09f45b1036381d21465ad
Leko32 <glebcocorici@gmail.com>
2025-05-03 15:33:42 +0300

    fix

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import asyncio
 import logging
 import os
-import threading
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -13,29 +11,13 @@ from dotenv import load_dotenv
 from admin_handlers import register_admin_handlers
 from handlers import register_user_handlers
 
-
-class HealthCheckHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot is running")
-
-
-def run_health_check_server():
-    server = HTTPServer(("0.0.0.0", 10000), HealthCheckHandler)
-    server.serve_forever()
-
-
-# Стартуем сервер в отдельном потоке
-threading.Thread(target=run_health_check_server, daemon=True).start()
-
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def main():
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(level=logging.ERROR)
 
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
